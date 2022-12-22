b401486c753c9ce15c120bc5f6fc8c14c6a89e12
Marcelo Henrique <Mhsf15@hotmail.com>
2022-12-22 12:16:09 -0300

    get musicas

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,44 @@
-async function  loadMusic(){
+var varGlobal = {
+    item: document.getElementById("container")
+}
+
+
+async function loadMusic(){
     const url = "./bdJson/Musicas.json"
     let response = await fetch(url);
     if(response.ok){
-        console.log(await response.json())
+        varGlobal.music = await response.json()
+        loadCardMusic(varGlobal.music.musicas)
+        console.log(varGlobal)
     }else{
         console.log("Error: "+ response.status)
     }
 }
 
 loadMusic();
+
+function loadCardMusic(musicas){
+var montaCard = ""
+    debugger
+   for (let i = 0; i < musicas.length; i++) {
+    montaCard += `
+    <div class="item" id="item">
+    <div class="card" id="card">
+    <img src="${musicas[i].UrlImg}" alt="" width="250">
+    <h5>${musicas[i].NameMusic}</h5>
+    <div class="listaBotao">
+    <button><i class="fa-solid fa-forward"></i></button>
+    <button onchange="${musicas[i].UrlMusic}"><i class="fa-solid fa-play"></i></button>
+    <button><i class="fa-solid fa-forward"></i></button>
+    </div>
+    <div class="listaBotao">
+    <input type="range">
+    </div>
+    </div>
+    </div>`
+        console.log(musicas[i])
+    
+   }
+   
+    varGlobal.item.innerHTML += montaCard
+}
